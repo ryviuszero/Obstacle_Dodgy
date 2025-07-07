f1bd89e7079ecddd83309bcbc2f7b6b2e40e2e45
mywind <ryviuszero@gmail.com>
2025-07-07 18:44:11 +0800

    Using SerializeField

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    float xValue = 0.01f;
-    float yValue = 0f;
-    float zValue = 0f;
+    [SerializeField]float xValue = 0.01f;
+    [SerializeField]float yValue = 0.01f;
+    [SerializeField]float zValue = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
